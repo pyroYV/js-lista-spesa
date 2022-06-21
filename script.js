7f7d0fceeb54d37d22a2ec6136599e8b2b0de67e
pyroYV <simonerizzo.pvt@gmail.com>
2022-06-21 14:44:45 +0200

    milestone

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const listaSpesa = ['aceto','mele','aglio','limonata','pringles']
-let i = 0 
-const contenitoreSpesa = document.querySelector(.lista-spesa)
-while (i > listaSpesa.length){
+const listaSpesa = ['aceto','mele','aglio','limonata','pringles'];
+let i = 0;
+const contenitoreSpesa = document.querySelector('.row .col-4 .lista-spesa');
+while (i < listaSpesa.length){
+    const elementoSpesa = document.createElement('li');
+    elementoSpesa.innerText = listaSpesa[i];
+    elementoSpesa.classList.add('fw-bolder')
+    contenitoreSpesa.append(elementoSpesa);
 
+    i++;
 }
